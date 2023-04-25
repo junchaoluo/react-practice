12dd266ed2d961e3ca743c918fa22ea23d3f72ae
junchaoluo@126.com <ccmzzzsyzxl110>
2023-04-25 10:20:09 +0800

    feat:初始化项目

---
+++ react-ts/vite.config.ts
@@ -0,0 +1,12 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    // 配置别名路径
+    '@': path.resolve(__dirname, './src')
+  }
+})
