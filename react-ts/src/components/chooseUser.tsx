250ef90a106614b1832946507c77d2f8a5e6c0bc
junchaoluo@126.com <ccmzzzsyzxl110>
2023-05-15 18:25:17 +0800

    feat:人员选择组件

--- react-ts/src/components/chooseUser.tsx
+++ react-ts/src/components/chooseUser.tsx
@@ -1,8 +1,18 @@
 import { memo, forwardRef } from 'react'
+import { Modal } from 'antd'
 
-const ChooseUser = memo(forwardRef((props, ref: HTMLElement) => {
+type IProps = {
+    visible: boolean, // 弹窗是否显示
+    checked?: Array<never>, // 已选中的
+    disabledList?: Array<never>, // 禁用的
+    departmentData: Array<never>, // 部门数据
+}
+
+const ChooseUser = memo(forwardRef((props: IProps, ref: any) => {
+    console.log(props, ref)
+    const {visible} = props
     return (
-        <div>人员</div>
+        <Modal open={visible} title="选择人员"></Modal>
     )
 }))
 
