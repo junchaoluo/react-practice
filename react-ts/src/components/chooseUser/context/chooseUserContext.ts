58b2719cfc0deee92c0e92ed88380a3af39fea2f
junchaoluo@126.com <ccmzzzsyzxl110>
2023-05-29 17:06:50 +0800

    feat: 完善新增项目页面逻辑

---
+++ react-ts/src/components/chooseUser/context/chooseUserContext.ts
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+import { SelectProps, DepartmentProps } from '@/types/chooseUser'
+
+type contextProps = {
+    checkedList?: Array<SelectProps>, // 已选中的
+    disabledList?: Array<SelectProps>, // 禁用的
+    userList?: Array<SelectProps>, // 禁用的
+    department?: Array<DepartmentProps>, // 部门数据
+    previousOptions?: Array<Array<DepartmentProps>>, // 部门数据
+    isSingle: boolean, // 是否单选
+    isDepartmentCheck?: boolean, // 是否可以选择部门
+    isUserCheck?: boolean, // 是否可以选择人员
+}
+
+const ChooseUseContext = createContext({
+    checkedList: [],
+    disabledList: [],
+    department: [],
+    previousOptions: [],
+    userList: [],
+    isSingle: false,
+    isDepartmentCheck: false,
+    isUserCheck: false,
+} as contextProps)
+
+export default ChooseUseContext
