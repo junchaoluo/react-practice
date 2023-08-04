db232890b6b4d5b5a3ef128836a5796e35985bbd
junchaoluo@126.com <ccmzzzsyzxl110>
2023-08-04 16:41:09 +0800

    feat:新增送样

--- react-ts/src/views/samplePool/editSample/index.tsx
+++ react-ts/src/views/samplePool/editSample/index.tsx
@@ -1,8 +1,22 @@
-import { memo, PropsWithChildren, FC } from 'react';
+import { memo, PropsWithChildren, FC, useState } from 'react';
 import style from './index.module.scss'
-import { Form, Select, Input, Row, Col } from 'antd'
+import { Form, Select, Input, Row, Col, DatePicker } from 'antd'
+
+const dateFormat = 'YYYY/MM/DD'
+const { Option } = Select
+
+type Compound = {
+    recordId: string,
+    name: string
+}
 
 const Template: FC<PropsWithChildren> = () => {
+    const [lastCompound, setLastCompound] = useState<Array<Compound>>([]) // 上次选择的化合物
+    const [compoundList, setCompoundList] = useState<Array<Compound>>([]) // 反应物和产物
+    // 清除化合物
+    const clearMaterialId = () => {
+    }
+
        return <div className={style.content}>
         <Form name="basic">
             <Row gutter={20}>
@@ -19,9 +33,33 @@ const Template: FC<PropsWithChildren> = () => {
                             </Form.Item>
                         </Col>
                         <Col span={6}>
-                            <Form.Item label="送样时间" name="samplesTime" rules={[{required: true, message: '请输入选择送样时间'}]}></Form.Item>
+                            <Form.Item label="送样时间" name="samplesTime" rules={[{required: true, message: '请输入选择送样时间'}]}>
+                                <DatePicker format={dateFormat}></DatePicker>
+                            </Form.Item>
                         </Col>
-                        <Col span={6}></Col>
+                        <Col span={6}>
+                            <Form.Item label="选择化合物" name="materialId">
+                                <Select allowClear defaultValue="" onClear={clearMaterialId} readonly>
+                                    {
+                                        lastCompound.map((item: Compound) => {
+                                            return <Option key={item.recordId} label={item.name} value={item.recordId}>
+                                                <span>上次选择</span>
+                                                <span>{item.name}</span>
+                                            </Option>
+                                        })
+                                    }
+                                    {
+                                        compoundList.map((item: Compound) => {
+                                            return <Option key={item.recordId} label={item.name} value={item.recordId}>
+                                                <span>{item.title}</span>
+                                                <span>{item.name}</span>
+                                            </Option>
+                                        })
+                                    }
+                                    <Option key="abc" value="">化合物选择</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
                     </Row>
                 </Col>
                 <Col span={4}></Col>
