e917a425a567c1f70407f06f7c74487e65dc8888
junchaoluo@126.com <ccmzzzsyzxl110>
2023-08-10 14:35:37 +0800

    feat:配置优化

--- react-ts/src/views/samplePool/editSample/components/compoundImg.tsx
+++ react-ts/src/views/samplePool/editSample/components/compoundImg.tsx
@@ -1,11 +1,22 @@
 import { memo, PropsWithChildren, FC } from 'react';
 import style from'../index.module.scss'
+import { Image } from 'antd'
 
-const Template: FC<PropsWithChildren> = () => {
+type IProps = PropsWithChildren<{
+    name: string
+}>
+
+const Template: FC<IProps> = (props) => {
+    const { name } = props
+
        return (
         <div className={style.compoundImg}>
-            <div className={style.image}></div>
-            <div className={style.info}></div>
+            <div className={style.image}>
+                <Image/>
+            </div>
+            <div className={style.info}>
+                { name ? name : '化合物预览' }
+            </div>
         </div>
        )
 }
