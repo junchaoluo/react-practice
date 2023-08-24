86cb0d5ffc224e9691d4e82dcfe9cbc1053a01dd
junchaoluo@126.com <ccmzzzsyzxl110>
2023-08-24 18:11:16 +0800

    fet

--- react-ts/src/views/samplePool/editSample/index.tsx
+++ react-ts/src/views/samplePool/editSample/index.tsx
@@ -2,17 +2,24 @@ import { memo, PropsWithChildren, FC, useState, Fragment } from 'react';
 import style from './index.module.scss'
 import BasicInfo from '@/views/samplePool/editSample/components/basicInfo';
 import DetectionInfo from '@/views/samplePool/editSample/components/detectionInfo';
+import { Button } from 'antd'
 
 const Template: FC<PropsWithChildren> = () => {
           return (
-        <div className={style.content}>
-            <div className={style.basicForm}>
-                <BasicInfo/>
+            <div className={style.container}>
+                <div className={style.content}>
+                    <div className={style.basicForm}>
+                        <BasicInfo/>
+                    </div>
+                    <div className={style.detection}>
+                        <DetectionInfo/>
+                    </div>
+                </div>
+                <div className={style.footer}>
+                    <Button type="primary">取消</Button>
+                    <Button type="primary">确定</Button>
+                </div>
             </div>
-            <div className={style.detection}>
-                <DetectionInfo/>
-            </div>
-        </div>
        )
 }
 
