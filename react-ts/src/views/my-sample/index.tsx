f645ef0d7aab26cd416cf6be88aa6ca57864d930
junchaoluo@126.com <ccmzzzsyzxl110>
2023-08-26 18:15:58 +0800

    feat:我的送样

--- react-ts/src/views/my-sample/index.tsx
+++ react-ts/src/views/my-sample/index.tsx
@@ -1,6 +1,12 @@
-import { PropsWithChildren, useEffect, useState } from 'react'
+import { PropsWithChildren, useCallback, useEffect, useState } from 'react'
 import './index.scss'
 import { Tabs, Form, Select, Input, Table, Row, Col } from 'antd'
+import type { ColumnsType } from 'antd/es/table'
+
+interface DataType {
+    index: number,
+    name: string
+}
 
 const MySample = (props: PropsWithChildren) => {
     const [tabList, setTabList] = useState([
@@ -60,9 +66,101 @@ const MySample = (props: PropsWithChildren) => {
         ]
     })
 
+    const columns: ColumnsType<DataType> = [
+        {
+            title: '序号',
+            dataIndex: 'index',
+            key: 'index',
+            width: 80,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '样品批号',
+            dataIndex: 'batchNo',
+            key: 'batchNo',
+            width: 250,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '化合物',
+            dataIndex: 'materialName',
+            key: 'materialName',
+            width: 150,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '当前状态',
+            dataIndex: 'samplesStatus',
+            key: 'samplesStatus',
+            width: 150,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '编号',
+            dataIndex: 'intNo',
+            key: 'intNo',
+            width: 150,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '检测项',
+            dataIndex: 'detectionName',
+            key: 'detectionName',
+            width: 150,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '检测部门',
+            dataIndex: 'deptName',
+            key: 'deptName',
+            width: 150,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '分析员',
+            dataIndex: 'analyzeName',
+            key: 'analyzeName',
+            width: 150,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '实验记录',
+            dataIndex: 'testRecord',
+            key: 'testRecord',
+            width: 150,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '分析完成日期',
+            dataIndex: 'analysisTime',
+            key: 'analysisTime',
+            width: 150,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '送样日期',
+            dataIndex: 'samplesTime',
+            key: 'samplesTime',
+            width: 150,
+            render: (text) => <a>{text}</a>
+        },
+        {
+            title: '操作',
+            dataIndex: 'action',
+            key: 'action',
+            fixed:'right',
+            width: 250,
+            render: (text) => <a>{text}</a>
+        },
+    ]
+
     useEffect(() => {
 
     }, [])
+
+    const onValuesChange = useCallback((changeValues, allValues) => {
+        console.log(changeValues, allValues)
+    }, [form])
 
     return (
         <div className='my-sample-contanier'>
@@ -71,42 +169,46 @@ const MySample = (props: PropsWithChildren) => {
                 <Tabs type='card' activeKey={activeKey} items={tabList} onChange={(activeKey) => setActiveKey(activeKey)}></Tabs>
             </div>
             <div className='my-sample-search'>
-                <Form form={form}>
+                <Form form={form} onValuesChange={onValuesChange}>
                     <Row gutter={20}>
                         <Col span={4}>
                             <Form.Item name='detectionItems'>
-                                <Select options={searchList.detectionItems} placeholder='请选择检测项'></Select>
+                                <Select allowClear={true} options={searchList.detectionItems} placeholder='请选择检测项'></Select>
                             </Form.Item>
                         </Col>
                         <Col span={4}>
                             <Form.Item name='depts'>
-                                <Select options={searchList.depts} placeholder='请选择检测部门'></Select>
+                                <Select allowClear={true} options={searchList.depts} placeholder='请选择检测部门'></Select>
                             </Form.Item>
                         </Col>
                         <Col span={4}>
                             <Form.Item name='analysts'>
-                                <Select options={searchList.analysts} placeholder='请选择分析员'></Select>
+                                <Select allowClear={true} options={searchList.analysts} placeholder='请选择分析员'></Select>
                             </Form.Item>
                         </Col>
                         <Col span={4}>
                             <Form.Item name='samplesStatuss'>
-                                <Select options={searchList.samplesStatuss} fieldNames={{label: 'name', value: 'value'}} placeholder='请选择检测状态'></Select>
+                                <Select allowClear={true} options={searchList.samplesStatuss} fieldNames={{label: 'name', value: 'value'}} placeholder='请选择检测状态'></Select>
                             </Form.Item>
                         </Col>
                         <Col span={4}>
                             <Form.Item name='samplesTimeType'>
-                                <Select options={searchList.samplesTimeType} placeholder='请选择送检时间'></Select>
+                                <Select allowClear={true} options={searchList.samplesTimeType} placeholder='请选择送检时间'></Select>
                             </Form.Item>
                         </Col>
                         <Col span={4}>
                             <Form.Item name='batchNo'>
-                                <Input placeholder='请输入样品批号查询'/>
+                                <Input allowClear={true} placeholder='请输入样品批号查询'/>
                             </Form.Item>
                         </Col>
                     </Row>
                 </Form>
             </div>
-            <div className='my-sample-table'></div>
+            <div className='my-sample-table'>
+                <Table scroll={{
+                    y: `calc(100vh - 116px)`
+                }} columns={columns} bordered={true} size='small'></Table>
+            </div>
         </div>
     )
 }
